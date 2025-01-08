2622506f3dba1c9a097d065faad3318663c9c082
Keyush nisar <nisarkeyush3@gmail.com>
2025-01-08 23:13:22 +0530

    trting

--- optimization/config/config.py
+++ optimization/config/config.py
@@ -3,7 +3,7 @@ import sqlite3
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "a_secret_key")
+    
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(os.path.dirname(BASE_DIR), 'instance', 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
